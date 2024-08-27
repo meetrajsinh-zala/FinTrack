cbdacc5f338b345f6e01eae5b56b9960205a272f
Meetrajsinh Zala <zalameetraj07@gmail.com>
2024-08-27 16:18:07 +0530

    Create Sidebar

--- FinTrack_FrontEnd/src/components/Dashboard/Dashboard.jsx
+++ FinTrack_FrontEnd/src/components/Dashboard/Dashboard.jsx
@@ -1,7 +1,59 @@
+import {
+  Home,
+  Landmark,
+  ReceiptText,
+  CircleDollarSign,
+  ArrowRightLeft,
+} from 'lucide-react';
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="hidden border-r bg-muted/40 md:block pt-20 pb-4 h-screen">
+      <div className="flex h-full max-h-screen flex-col gap-2">
+        <div className="flex-1">
+          <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+            <Link
+              href="#"
+              className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+            >
+              <Home className="h-4 w-4" />
+              Home
+            </Link>
+            <Link
+              href="#"
+              className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+            >
+              <CircleDollarSign className="h-4 w-4" />
+              My Bank
+            </Link>
+            <Link
+              href="#"
+              className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+            >
+              <ReceiptText className="h-4 w-4" />
+              Transaction History
+            </Link>
+            <Link
+              href="#"
+              className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+            >
+              <ArrowRightLeft className="h-4 w-4" />
+              Payment Transfer
+            </Link>
+            <Link
+              href="#"
+              className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+            >
+              <Landmark className="h-4 w-4" />
+              Connect Bank
+            </Link>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
